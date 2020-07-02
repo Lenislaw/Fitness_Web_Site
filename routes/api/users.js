68ea7ad906011486f721b7a9602f6abf6c87cf6a
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-07-02 20:41:18 +0200

    Add Register User Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 // @route  POST api/users
 // @desc   Register User
@@ -22,14 +27,51 @@ router.post(
   ],
   async (req, res) => {
     // Check request validation
-    console.log(req.body);
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    // Destructuring
+    const { email, name, surname, password } = req.body;
+
     try {
-      res.status(200).json({ msg: "User Get Works" });
+      // Check duplicate user in DB
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exist" }] });
+      }
+      // New User model
+      user = new User({
+        name,
+        surname,
+        email,
+        password,
+      });
+
+      // Bcrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: "5 days" },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
